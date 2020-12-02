4ccb96e6ec1e297fac16bfea32e5169f678dc21c
sarakhandaker <kttsara@gmail.com>
2020-12-01 18:38:06 -0800

    converting userstops component to functional

--- transitapp-spa/src/components/UserStops.js
+++ transitapp-spa/src/components/UserStops.js
@@ -1,22 +1,22 @@
-import React, { Component } from 'react'
+import React, { Component, useState, useEffect } from 'react'
 import { api } from '../services/api';
+import { useJwt } from "react-jwt";
+const token = localStorage.getItem('token');
 
-class UserStops extends Component {
-    state = {
-        user: []
+const UserStops = props => {
+    const { decodedToken } = useJwt(token)
+    let [user, setUser]= useState([])
+
+    const getUsers= () => {
+        const id = decodedToken.nameid
+        api.stop.getUserStops(id).then(r => setUser(r.data))
     }
 
-    getUsers() {
-        const id = this.props.decodedToken.nameid
-        api.stop.getUserStops(id).then(r =>
-            this.setState({ user: r.data }))
-    }
-
-    componentDidUpdate() {
-            this.getUsers()
+    const useEffect= ()=> {
+        getUsers()
     }
     
-    handleDelete = id => {
+    const handleDelete = id => {
         api.handleDelete(id).then( r => {
             let stops=this.state.user.UserStops.filter(stop => stop.id !== id)
             let user={...this.state.user, UserStops: stops}
@@ -25,30 +25,25 @@ class UserStops extends Component {
         )
     }
 
-
-    makeList = () => {
+    const makeList = () => {
         return this.state.user.userStops.map(stop =>
             <li key={stop.id} className="mb-3">
                 <div className="row" >
                     <i className="fa fa-bus mr-2"></i>
                     <p><strong>{stop.label}</strong> - {stop.stop.stopName}</p>
                 </div>
-                <button onClick={()=> this.handleDelete(stop.id)} className="btn btn-success btn-sm mr-auto">Delete Stop</button>
+                <button onClick={()=> handleDelete(stop.id)} className="btn btn-success btn-sm mr-auto">Delete Stop</button>
             </li>)
     }
 
-    render() {
-        const stops = this.state.user.userStops
-        return (
-            <div className="container mt-3 pt-3 mb-3" style={{ "border": "solid", "backgroundColor": "rgba(255, 255, 255,0.8)" }}>
+    return (<div className="container mt-3 pt-3 mb-3" style={{ "border": "solid", "backgroundColor": "rgba(255, 255, 255,0.8)" }}>
                 <h3> Your Saved Bus Stops: </h3>
                 <hr />
                 <ul style={{ "listStyle": "none" }}>
-                    {stops ? this.makeList() : null}
+                    {user.stops ? makeList() : null}
                 </ul>
             </div>
         );
-    }
 }
 
 export default UserStops;
