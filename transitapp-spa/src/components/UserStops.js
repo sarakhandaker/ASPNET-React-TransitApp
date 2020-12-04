9cf58fc5564459e6ce6090d15495e58065ca8cae
sarakhandaker <kttsara@gmail.com>
2020-12-04 10:09:54 -0800

    added map to user stops

--- transitapp-spa/src/components/UserStops.js
+++ transitapp-spa/src/components/UserStops.js
@@ -1,17 +1,17 @@
 import React, { useState, useEffect } from 'react'
 import { api } from '../services/api';
+import LeafletMap from './LeaftletMap'
 
 const UserStops = props => {
     let [userStops, setUserStops] = useState([])
 
     useEffect(() => {
-        if (props.id) 
+        if (props.id)
             api.stop.getUserStops(props.id).then(r => setUserStops(r.data))
     }, [props.id])
 
     const handleDelete = id => {
         api.stop.deleteStop(props.id, id)
-        debugger
         setUserStops(userStops.filter(stop => stop.id !== id))
     }
 
@@ -27,11 +27,16 @@ const UserStops = props => {
     }
 
     return (<div className="container mt-3 pt-3 mb-3" style={{ "border": "solid", "backgroundColor": "rgba(255, 255, 255,0.8)" }}>
-        <h3> Your Saved Bus Stops: </h3>
-        <hr />
-        <ul style={{ "listStyle": "none" }}>
-            {userStops ? makeList() : null}
-        </ul>
+        <div className="col-sm-8">
+            <h3> Your Saved Bus Stops: </h3>
+            <hr />
+            <ul style={{ "listStyle": "none" }}>
+                {userStops ? makeList() : null}
+            </ul>
+        </div>
+        <div className="col-sm-8">
+            <LeafletMap address={props.address} stops={userStops? userStops.map(s=> s.stop): null}></LeafletMap>
+        </div>
     </div>
     );
 }
