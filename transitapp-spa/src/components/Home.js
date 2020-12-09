cf77e645979974c4bd41ce8e6f71e24092be6ab4
sarakhandaker <kttsara@gmail.com>
2020-12-09 13:18:49 -0800

    wording

--- transitapp-spa/src/components/Home.js
+++ transitapp-spa/src/components/Home.js
@@ -10,7 +10,7 @@ const Home = () => {
             { !decodedToken ? <Register /> :
                 null}
             <div className="row mt-3 p-5 text-center justify-content-center">
-                <h4>This website is not affiliated with King County Metro. It uses GFTS data to help users find and save bus stops. For more information check out the King County Metro Website Below:</h4>
+                <h4>This website is not affiliated with King County Metro. It uses GFTS data to help users find and save bus stops. For more information check out the King County Metro Website below:</h4>
                 <br />
                 <a href="https://kingcounty.gov/depts/transportation/metro.aspx" className="btn btn-danger btn-raised btn-lg mt-3"><i className="fa fa-play"></i> Go To King County Metro Website</a>
             </div>
