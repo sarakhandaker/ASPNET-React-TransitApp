c03ba0fc3d4104b80aafcc3861b2476be7b97672
sarakhandaker <kttsara@gmail.com>
2020-12-09 13:45:00 -0800

    added stoptime model

---
+++ TransitApp.API/Models/StopTime.cs
@@ -0,0 +1,18 @@
+using System;
+
+namespace TransitApp.API.Models
+{
+    public class StopTime
+    {
+    public int Id { get; set; }
+    public int TripId { get; set; }
+    public DateTime ArrivalTime { get; set; }
+    public DateTime DepartureTime { get; set; }
+    public int StopId { get; set; }
+    public string StopSequence { get; set; }
+    public string StopHeadsign { get; set; }
+    public string PickupType { get; set; }
+    public string DropOffType { get; set; }
+    public double ShapeDistTraveled { get; set; }
+    }
+}
