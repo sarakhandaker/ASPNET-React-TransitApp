f7aca6405457305db9c7741f6511686278ff11ae
sarakhandaker <kttsara@gmail.com>
2020-12-10 12:08:09 -0800

    date over 24 resolved

--- TransitApp.API/Data/writeStopTimesData.rb
+++ TransitApp.API/Data/writeStopTimesData.rb
@@ -8,9 +8,25 @@ data_arr.shift
 data_arr.each do |each_stop|
     each_stop=each_stop.split(",")
     each_stop[2]=each_stop[2][1...-1]
+
+    # ArrivalTimeLogic
+    arrivalTime=each_stop[1]
+    if (arrivalTime[0]+ arrivalTime[1]).to_i >= 24
+        hour=(((arrivalTime[0]+arrivalTime[1]).to_i)-24)
+        if (hour>9)
+            hour=hour.to_s
+            arrivalTime[0]=hour[0]
+            arrivalTime[1]=hour[1]
+        else 
+            hour=hour.to_s
+            arrivalTime[1]=hour[0]
+            arrivalTime[0]="0"
+        end
+    end
+
     stopObjects.push({
         "TripId": each_stop[0].to_i, 
-        "ArrivalTime": each_stop[1], 
+        "ArrivalTime": arrivalTime, 
         # "DepartureTime": each_stop[2], 
         "StopId": each_stop[3].to_i, 
         "StopSequence": each_stop[4], 
