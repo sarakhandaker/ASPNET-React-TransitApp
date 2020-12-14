00ac6729d9e028a7bcdee8dfcd6c57f254046086
sarakhandaker <kttsara@gmail.com>
2020-12-14 12:06:24 -0800

    addded show times button

--- transitapp-spa/src/components/Presentational/SingleUserStop.js
+++ transitapp-spa/src/components/Presentational/SingleUserStop.js
@@ -1,6 +1,14 @@
 import React, { Component } from 'react'
 
 export default class SingleUserStop extends Component {
+    state= {
+        showtimes: false
+    }
+
+    showTimes=()=>[
+
+    ]
+
     render() {
         return (
             <li className="mb-3">
@@ -9,6 +17,8 @@ export default class SingleUserStop extends Component {
                     <p><strong>{this.props.stop.label}</strong> - {this.props.stop.stop.stopName}</p>
                 </div>
                 <button onClick={() => this.props.handleDelete(this.props.stop.id)} className="btn btn-success btn-sm mr-auto">Delete Stop</button>
+                <button onClick={this.showTimes} className="btn btn-success btn-sm mr-auto ml-2">Show Times</button>
+
             </li>
         )
 }
