b97b786cb38336e9f47169828a1b73073188e0a9
sarakhandaker <kttsara@gmail.com>
2020-12-15 14:50:19 -0800

    added gettimes to itransit

--- TransitApp.API/Data/ITransitRepository.cs
+++ TransitApp.API/Data/ITransitRepository.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections.Generic;
 using System.Threading.Tasks;
 using TransitApp.API.Models;
@@ -6,16 +7,14 @@ namespace TransitApp.API.Data
 {
     public interface ITransitRepository
     {
-         void Add<T>(T entity) where T: class;
-         void Delete<T>(T entity) where T: class;
-
-         Task<bool> SaveAll();
-
-         Task<IEnumerable<Stop>> GetClosestStops(double lat, double lon);
-
-         Task<User> GetUser(int id);
+        void Add<T>(T entity) where T: class;
+        void Delete<T>(T entity) where T: class;
+        Task<bool> SaveAll();
+        Task<IEnumerable<Stop>> GetClosestStops(double lat, double lon);
+        Task<User> GetUser(int id);
         Task<UserStop> GetUserStop(int id);
-         Task<Stop> GetStop(int id);
+        Task<Stop> GetStop(int id);
 
+        Task<IEnumerable<DateTime>> GetTimes(int stopId);
     }
 }
