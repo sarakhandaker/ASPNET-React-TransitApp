f4b7d1beb3e6a2956fb2ac3f5f353538cb778e08
sarakhandaker <kttsara@gmail.com>
2020-12-16 12:19:55 -0800

    added stop times button function

--- transitapp-spa/src/components/Presentational/SingleUserStop.js
+++ transitapp-spa/src/components/Presentational/SingleUserStop.js
@@ -1,12 +1,13 @@
 import React, { Component } from 'react'
+import {api} from '../../services/api'
 
 export default class SingleUserStop extends Component {
     state= {
         showtimes: false
     }
 
-    showTimes=()=>[
-
+    showTimes= id =>[
+        api.stop.times(id).then( r => console.log(r))
     ]
 
     render() {
@@ -17,7 +18,7 @@ export default class SingleUserStop extends Component {
                     <p><strong>{this.props.stop.label}</strong> - {this.props.stop.stop.stopName}</p>
                 </div>
                 <button onClick={() => this.props.handleDelete(this.props.stop.id)} className="btn btn-success btn-sm mr-auto">Delete Stop</button>
-                <button onClick={this.showTimes} className="btn btn-success btn-sm mr-auto ml-2">Show Times</button>
+                <button onClick={() => this.showTimes(this.props.stop.id)} className="btn btn-success btn-sm mr-auto ml-2">Show Times</button>
 
             </li>
         )
