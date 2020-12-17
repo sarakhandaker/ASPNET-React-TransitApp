071f17b81bda1127f5c72067f4828faffa2e765d
sarakhandaker <kttsara@gmail.com>
2020-12-17 12:36:25 -0800

    created distance formula

--- transitapp-spa/src/components/Presentational/Results.js
+++ transitapp-spa/src/components/Presentational/Results.js
@@ -19,10 +19,7 @@ class Results extends Component {
     }
 
     clickSave = id => {
-        this.setState({
-            id: id,
-            showModal: true
-        })
+        this.setState({ id: id, showModal: true})
     }
 
     makeList = () => {
@@ -38,6 +35,17 @@ class Results extends Component {
                     </div>
                 </div>
             </li>)
+    }
+
+    getDistanceFromLatLonInKm = (lat1, lon1, lat2, lon2) => {
+        const dLat = (lat2 - lat1) * (Math.PI / 180) // deg2rad
+        const dLon = (lon2 - lon1) * (Math.PI / 180)
+        var a = Math.sin(dLat / 2) * Math.sin(dLat / 2) +
+            Math.cos((lat1) * (Math.PI / 180)) * Math.cos((lat2) * (Math.PI / 180)) *
+            Math.sin(dLon / 2) * Math.sin(dLon / 2)
+        var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
+        var d = 6371 * c; // Radius of the earth in km-> Distance in km
+        return d;
     }
 
     render() {
